2df9a1b49473db260a51513ab281fe8691f472cc
Kanogor <the.kanogor@yandex.ru>
2022-08-03 22:05:04 +0300

    It just start)

---
+++ app/src/main/java/ru/kanogor/abra_kadabra/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.kanogor.abra_kadabra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
